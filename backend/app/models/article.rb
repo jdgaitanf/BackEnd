1adf1d374e552660d87f763409298e9950c3f81f
Juan D. Gaitan <jdgaitanf@unal.edu.co>
2018-03-28 18:03:53 -0500

    Asociaciones terminadas

--- backend/app/models/article.rb
+++ backend/app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :classification
+  has_many :photos
+  has_many :questions
+  has_many :sales
 end
--- backend/app/models/sale.rb
+++ backend/app/models/sale.rb
@@ -2,4 +2,5 @@ class Sale < ApplicationRecord
   has_one :seller, :class_name => "User"
   has_one :buyer, :class_name => "User"
   belongs_to :article
+  has_many :scores
 end
